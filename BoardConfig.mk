274224580b067748b49e9702cf1bc9d2fcee9878
Greg Wallace <greg@gregtwallace.com>
2017-01-22 12:58:11 -0500

    d800: use gcc 4.8 toolchain
    * d800 does not boot with default.
    
    Change-Id: I9d164fe1457f0159c70362f9008f3ab0269f91fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,8 @@
 include device/lge/g2-common/BoardConfigCommon.mk
 
 TARGET_KERNEL_CONFIG := crdroid_d800_defconfig
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
 TARGET_OTA_ASSERT_DEVICE := d800,g2,galbi
 
